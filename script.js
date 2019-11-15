9ba71a9e907c1f4b6a744ecb5319ae99126b005f
Vanitas Noctis <apple@apples-mbp.wifi.bcit.ca>
2019-11-15 15:14:22 -0800

    change from instaTam to Instahearts

---
+++ script.js
@@ -0,0 +1,47 @@
+console.log(posts);
+
+const createPostElement = (apost) => {
+    return dom element
+}
+
+const newDiv= document.createElement('div');
+newDiv.classList.add("section");
+const newDivAuth= document.createElement('div');
+newDivAuth.classList.add("author");
+const newOrder= document.createElement('ol');
+newOrder.classList.add("section");
+const newList= document.createElement('li');
+newList.classList.add("section");
+const newUnorder= document.createElement('ul');
+newUnorder.classList.add("section");
+const newFigure= document.createElement('figure');
+newFigure.classList.add("section");
+const newImg= document.createElement('img');
+newImg.classList.add("section");
+const newFigcap= document.createElement('figcaption');
+newFigcap.classList.add("section");
+const newTheI= document.createElement('i');
+newTheI.classList.add("section");
+const newForm= document.createElement('form');
+newForm.classList.add("section");
+const newInput= document.createElement('input');
+newInput.classList.add("section");
+
+/*let former = document.getElementById('form1');
+let event = former.addEventListener('submit', function(evt){
+    evt.preventDefault();
+    let msg = document.getElementById("input1").value;
+    let commenter = document.querySelector(".comment");
+   commenter.innerText
+    console.log(text);
+})*/
+
+
+const postImage = document.createElement('img');
+postImage.classList.add("post-image");
+postImage.setAttribute("src", post.image_url);
+imageContainer.appendChild(postImage);
+
+//let createPostElement = function() {
+    
+//}
